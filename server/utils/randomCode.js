682e5098eb400c7833d8280da413774f163944a2
kylezap <kzapcic+github@gmail.com>
2024-07-01 13:43:37 -0400

    add random code generator helper

---
+++ server/utils/randomCode.js
@@ -0,0 +1,12 @@
+//Generates a random string of 4 characters
+
+function generateRandomCode() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let code = '';
+    for (let i = 0; i < 4; i++) {
+        code += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return code;
+}
+
+module.exports = generateRandomCode;

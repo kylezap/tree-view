6d7ac7ac10d89aaf47947576dadd47393ad83747
kylezap <kzapcic+github@gmail.com>
2024-07-03 17:18:16 -0400

    adjust server for production static files

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const path = require('path');
 const cors = require('cors');
 
 // import sequelize connection
@@ -18,6 +19,14 @@ const PORT = process.env.PORT || 3001;
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+} 
 
 // turn on routes
 app.use(routes);

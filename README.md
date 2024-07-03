345e5695e71a9de650e2bdbf4e8f4cd59ef5d964
kylezap <kzapcic+github@gmail.com>
2024-07-03 16:47:59 -0400

    readme and vite config updated

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# tree-view
+# Tree View
+
+A tree containing group of factories generated from a root. You can generate as many factories as you'd like. From the these factoreies, a user can generate a a list of up to 15 numbers, also being able to set the range these numbers fall into.
+
+<!-- insert image -->
+
--- client/vite.config.js
+++ client/vite.config.js
@@ -7,4 +7,13 @@ dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:10000', // The target server to proxy to
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api prefix when forwarding to the target
+      },
+    },
+  },
+});

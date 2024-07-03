50acf894033b7cf80b4b2eea30e60ea76bddd3f0
kylezap <kzapcic+github@gmail.com>
2024-07-03 19:42:24 -0400

    image to readme

--- server/config/connection.js
+++ server/config/connection.js
@@ -10,9 +10,7 @@ if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost', // Change to your actual host
     dialect: 'postgres',
-    dialectOptions: {
-      decimalNumbers: true,
-    },
+     // Change to your actual
   });
 }
 

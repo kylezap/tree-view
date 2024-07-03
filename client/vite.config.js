6dcb5cf31400dbbbc3b3a7582f237af1a91692db
kylezap <kzapcic+github@gmail.com>
2024-07-03 17:35:25 -0400

    test update for vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,13 +7,4 @@ dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:10000', // The target server to proxy to
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api prefix when forwarding to the target
-      },
-    },
-  },
-});
+})
